fd27bb4fdf4a0cc521950f7b443fac36521ecccc
fernandomatheus <fernandomatheuscl@gmail.com>
2015-06-01 11:42:08 -0300

    Create TopicoDAO.java

---
+++ Documentos/Fernando-Matheus/Gerar-Prova/DAO/TopicoDAO.java
@@ -0,0 +1,67 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javabeans.Topico;
+import conexao.Conexao;
+
+public class TopicoDAO {
+	
+	private Connection con = Conexao.getConnection();
+	
+	public String buscarTopico(int pk_idDisciplina){
+		
+			String sql ="SELECT * FROM topico WHERE Disciplina_idDisciplina = ?";
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, pk_idDisciplina);
+			
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+                int idT = rs.getInt("idTopico");
+				String nomeT = rs.getString("nome");
+				int idFK = rs.getInt("Disciplina_idDisciplina");
+                
+                return nomeT;
+                
+            }            
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		return null;
+	}
+	
+	public List<Topico> buscarLista(){
+		String sql = "SELECT * FROM topico";
+		
+		List<Topico> lista = new ArrayList<Topico>();
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet resultados = stmt.executeQuery(); 
+			
+			while(resultados.next()){
+				int idT = resultados.getInt("idTopico");
+				String nomeT = resultados.getString("nome");	
+				int fk_id_d = resultados.getInt("Disciplina_idDisciplina");
+				Topico topico = new Topico(idT, nomeT, fk_id_d);
+				lista.add(topico);
+				
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro - "+e.getMessage());
+		}return lista;
+		
+		
+	}
+
+}
